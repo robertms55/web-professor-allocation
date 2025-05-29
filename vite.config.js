465211cb8dea686c6b6fcad946524249b812206f
robertms15 <roberto.moreira99@gmail.com>
2025-05-29 08:27:03 -0300

    Ajuste arquivo manisfet para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
   test: {
     globals: true,
